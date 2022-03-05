a436f37a2e9c7f4d6680da0d5cebbc082a4a494d
Patrick Morton <patrickjamesmorton@gmail.com>
2022-03-05 15:14:58 +0000

    removed Greeter deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,20 +8,15 @@ async function main() {
     deployer.address
   );
 
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, World!");
-
   const Token = await hre.ethers.getContractFactory("ERC20Token");
   const token = await Token.deploy("Test Token", "TT", 0, 1000000, deployer.address);
 
   const TokenFactory = await hre.ethers.getContractFactory("ERC20TokenFactory");
   const tokenFactory = await TokenFactory.deploy();
 
-  await greeter.deployed();
   await token.deployed();
   await tokenFactory.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
   console.log("Token deployed to:", token.address);
   console.log("Token Factory deployed to:", tokenFactory.address);
 }
